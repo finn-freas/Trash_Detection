3950465b62084656f76a168615d9be2c15438c1a
finn-freas <finn.freas@gmail.com>
2025-06-09 19:57:36 -0400

    update

---
+++ Inference.py
@@ -0,0 +1,17 @@
+import torch
+import cv2
+
+# Load YOLOv5 model from local weights
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='Best_Weights.pt', force_reload=True)
+model.conf = 0.3  # set confidence threshold (I recommend 0.3)
+
+# Load image
+img = 'img.png'  # relative path to your test image
+
+# Run inference
+results = model(img)
+
+# Results
+results.print()
+results.show()  
+results.save()  # saves results in runs/detect/exp by default
